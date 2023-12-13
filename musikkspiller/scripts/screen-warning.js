9606303c7367387b60d4ef2649a9943e086aa3d9
SteelyxYT <91285184+SteelyxYT@users.noreply.github.com>
2023-12-13 09:03:34 +0100

    Good enough

---
+++ musikkspiller/scripts/screen-warning.js
@@ -0,0 +1,4 @@
+if (window.innerWidth <= 248){
+    console.warn("Nettsiden er ikke optimalisert for din skjerm størrelse")
+    alert("Nettsiden er ikke optimalisert for din skjerm størrelse. Problemer kan oppstå.")
+}
